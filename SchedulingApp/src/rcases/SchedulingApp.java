a9c086357c01e3e607300768a93a6a8411cfc80a
RBertoCases <roberto.cases@gmail.com>
2018-01-15 20:43:30 -0800

    Worked on SchedulingApp

--- SchedulingApp/src/rcases/SchedulingApp.java
+++ SchedulingApp/src/rcases/SchedulingApp.java
@@ -19,7 +19,7 @@ public class SchedulingApp extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/rcases/view/Login.fxml"));
         
         Scene scene = new Scene(root);
         
