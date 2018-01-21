d868af468a7e334fc63eaab8770e6a5c479e95d7
RBertoCases <roberto.cases@gmail.com>
2018-01-21 12:13:45 -0800

    Refactored SchedulingApp, Login allows Cancel to close program

--- SchedulingApp/src/rcases/SchedulingApp.java
+++ SchedulingApp/src/rcases/SchedulingApp.java
@@ -5,19 +5,56 @@
  */
 package rcases;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import rcases.view.LoginController;
 
 /**
  *
- * @author rober
+ * @author rcases
  */
 public class SchedulingApp extends Application {
     
+    private Stage primaryStage;
+    private AnchorPane loginScreen;
+    
     @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Scheduling Application");
+
+        showLoginScreen();
+    }
+    
+    public void showLoginScreen() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(SchedulingApp.class.getResource("/rcases/view/Login.fxml"));
+            loginScreen = (AnchorPane) loader.load();   
+
+            // Give the controller access to the main app.
+            LoginController controller = loader.getController();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(loginScreen);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+    
+    /*@Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/rcases/view/Login.fxml"));
         
--- SchedulingApp/src/rcases/view/LoginController.java
+++ SchedulingApp/src/rcases/view/LoginController.java
@@ -1,35 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rcases.view;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-/**
- *
- * @author rober
- */
-public class LoginController implements Initializable {
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
     
     @FXML
-    private Label label;
+    private Label errorMessage;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    void handleSignInAction(ActionEvent event) {
+
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML
+    void handleCancelAction(ActionEvent event) {
+        System.exit(0);
+    }
+
 }
