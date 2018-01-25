2d32d4cda5866d7e336bd100cbebd102f0ef1a5f
RBertoCases <roberto.cases@gmail.com>
2018-01-25 09:16:55 -0800

    Refactored SchedulingApp, Login allows Cancel to close program

--- SchedulingApp/src/rcases/view/LoginController.java
+++ SchedulingApp/src/rcases/view/LoginController.java
@@ -19,7 +19,21 @@ public class LoginController {
     
     @FXML
     void handleSignInAction(ActionEvent event) {
-
+    /**template code to be altered to JavaFX
+        if(jTextField1.getText().length()==0)  // Checking for empty field
+            JOptionPane.showMessageDialog(null, "Empty fields detected ! Please fill up all fields");
+        else if(jPasswordField1.getPassword().length==0)  // Checking for empty field
+            JOptionPane.showMessageDialog(null, "Empty fields detected ! Please fill up all fields");
+        else{
+            String user = jTextField1.getText();   // Collecting the input
+            char[] pass = jPasswordField1.getPassword(); // Collecting the input
+            String pwd = String.copyValueOf(pass);  // converting from array to string
+            if(validate_login(user,pwd))
+                JOptionPane.showMessageDialog(null, "Correct Login Credentials");        
+            else
+                JOptionPane.showMessageDialog(null, "Incorrect Login Credentials");
+            }
+        */
     }
     
     @FXML
