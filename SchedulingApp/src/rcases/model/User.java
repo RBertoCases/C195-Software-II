d3c5ac4381a627f9405fe45e7d6f64f82c5b5d9e
RBertoCases <roberto.cases@gmail.com>
2018-01-30 13:31:45 -0800

    added constructer, getters, and setters to User

--- SchedulingApp/src/rcases/model/User.java
+++ SchedulingApp/src/rcases/model/User.java
@@ -13,7 +13,46 @@ public class User {
     private int userID;
     private String username;
     private String password;
-    private boolean active;
+    private int active;
+
+    public User(int userID, String username, String password, int active) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.active = active;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
     
     
 }
