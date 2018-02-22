2a9fe45c8efd4a40868541cd61f1af24173d4db7
rcases <rcases@WRWCNOCCA01W05.corp.adcinternal.com>
2018-02-22 09:06:25 -0800

    working on AppointmentScreens

--- SchedulingApp/src/rcases/view/AppointmentScreenController.java
+++ SchedulingApp/src/rcases/view/AppointmentScreenController.java
@@ -5,7 +5,14 @@
  */
 package rcases.view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
 import rcases.SchedulingApp;
+import rcases.model.Appointment;
 
 /**
  * FXML Controller class
@@ -15,9 +22,58 @@ import rcases.SchedulingApp;
 public class AppointmentScreenController {
     
     private SchedulingApp mainApp;
+    
+    @FXML
+    private TableView<Appointment> ApptTableView;
 
+    @FXML
+    private TableColumn<Appointment, ?> startApptColumn;
+
+    @FXML
+    private TableColumn<Appointment, ?> endApptColumn;
+    
+     @FXML
+    private TableColumn<Appointment, ?> titleApptColumn;
+
+    @FXML
+    private TableColumn<Appointment, ?> typeApptColumn;
+
+    @FXML
+    private TableColumn<Appointment, ?> customerApptColumn;
+
+    @FXML
+    private TableColumn<Appointment, ?> consultantApptColumn;
+
+    @FXML
+    private RadioButton weekRadioButton;
+
+    @FXML
+    private RadioButton monthRadioButton;
+    
+    @FXML
+    private ToggleGroup apptToggleGroup;
+    
     public void setAppointmentScreen(SchedulingApp mainApp) {
 	this.mainApp = mainApp;
+        apptToggleGroup = new ToggleGroup();
+        this.weekRadioButton.setToggleGroup(apptToggleGroup);
+        this.monthRadioButton.setToggleGroup(apptToggleGroup);
     }
+
+    @FXML
+    void handleDeleteAppt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEditAppt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleNewAppt(ActionEvent event) {
+
+    }
+    
     
 }
